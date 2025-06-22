03cf6edd6d2b64a74f67cbc5a02a1e90ce5bc43f
iusss <aayushman950@gmail.com>
2025-06-22 23:36:20 +0545

    loads the dataset

---
+++ cyclegan/dataset_loader.py
@@ -0,0 +1,20 @@
+import pathlib
+import tensorflow as tf
+
+
+def load_image(image_path):
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    # image = tf.image.convert_image_dtype(image, tf.float32)
+
+    return image
+
+
+def image_folder_to_dataset(folder_path, buffer_size=1000):
+    path = pathlib.Path(folder_path)
+    files = list(map(lambda a: str(a).encode('utf-8'), path.glob('*.jpg')))
+
+    dataset = tf.data.Dataset.from_tensor_slices(files)
+    dataset = dataset.map(load_image, num_parallel_calls=tf.data.experimental.AUTOTUNE).shuffle(buffer_size)
+
+    return dataset
